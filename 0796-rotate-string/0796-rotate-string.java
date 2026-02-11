f423745b06b72afd0a58defedf25980963dbce1a
Mehak-khann <mehakkhann2201@gmail.com>
2026-02-11 23:15:13 +0530

    Update 0796-rotate-string.java

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,3 +1,4 @@
+//approach 1
 class Solution {
     public boolean rotateString(String s, String goal) {
         int n=s.length();
@@ -8,6 +9,25 @@ class Solution {
         for(int i=1;i<=n;i++){
             s=s.substring(1) + s.charAt(0);
             if(s.equals(goal)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//approach 2
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n=s.length();
+        int m=goal.length();
+        if(n!=m){
+            return false;
+        }
+        for(int i=1;i<=n;i++){
+           // s=s.substring(1) + s.charAt(0);
+
+            if((s+s).contains(goal)){//try using kmp algorithm
                 return true;
             }
         }
