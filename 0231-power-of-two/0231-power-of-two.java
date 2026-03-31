d18fb6bf17766dbc89b9947bc96d8e0a3ef9d3c2
Mehak-khann <mehakkhann2201@gmail.com>
2026-03-31 16:23:16 +0530

    Time: 1 ms (96.5%), Space: 42.9 MB (11.43%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,24 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-     if(n<=0)    return false;
-     while(n%2==0){
-        n=n/2;
-     }
-     if(n==1) return true;
-     return false;
+        //iterative
+    //     public boolean isPowerOfTwo(int n) {
+    //  if(n<=0)    return false;
+    //  while(n%2==0){
+    //     n=n/2;
+    //  }
+    //  if(n==1) return true;
+    //  return false;
+    //     }
+    
+        //recursive
+        if(n==1){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        if(n%2==0)
+        return  isPowerOfTwo(n/2);
+        else return false;
     }
 }
