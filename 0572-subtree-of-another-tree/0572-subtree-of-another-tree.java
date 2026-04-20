cdd0ebf2266697e3276915dbc8704857074fbe3d
Mehak-khann <mehakkhann2201@gmail.com>
2026-04-20 12:01:30 +0530

    Time: 6 ms (12.99%), Space: 46.6 MB (21.68%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -13,25 +13,66 @@
  *     }
  * }
  */
-class Solution {
-    public void serialize(TreeNode root,StringBuilder sb){
-        if(root==null){
-            sb.append(",#,");
-            return;
-        }
-        sb.append(",");
-        sb.append(root.val);
-        sb.append(",");
 
-        serialize(root.left,sb);
-        serialize(root.right,sb);
+ //approach 1
+
+//   public void serialize(TreeNode root,StringBuilder sb){
+//         if(root==null){
+//             sb.append(",#,");
+//             return;
+//         }
+//         sb.append(",");
+//         sb.append(root.val);
+//         sb.append(",");
+
+//         serialize(root.left,sb);
+//         serialize(root.right,sb);
+//     }
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         StringBuilder t=new StringBuilder();
+//         StringBuilder s=new StringBuilder();
+//         serialize(root,t);
+//         serialize(subRoot,s);
+//         if(t.toString().contains(s.toString())){
+//             return true;
+//         }
+//         return false;
+//     }
+
+//approach 2
+class Solution {
+    public boolean identical(TreeNode p,TreeNode q){
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        boolean ans1=identical(p.left,q.left);
+        boolean ans2=identical(p.right,q.right);
+
+        if(ans1==true && ans2==true){
+            return true;
+        }
+        return false;
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        StringBuilder t=new StringBuilder();
-        StringBuilder s=new StringBuilder();
-        serialize(root,t);
-        serialize(subRoot,s);
-        if(t.toString().contains(s.toString())){
+        if(subRoot==null) return true;
+        if(root==null)return false;
+      
+        boolean ans1=identical(root,subRoot);
+        
+        if(ans1 ==true){
+            return true;
+        }
+
+        boolean left=isSubtree(root.left,subRoot);
+        boolean right = isSubtree(root.right,subRoot);
+
+        if(left==true || right==true){
             return true;
         }
         return false;
