76e393970c7e647da79ed5f1b0c8cb354fc5934e
Mehak-khann <mehakkhann2201@gmail.com>
2026-06-09 15:00:01 +0530

    Time: 1 ms (100%), Space: 62.5 MB (20.14%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
-        long ans=-1;
+        long ans=0;
         int n=nums.length;
         int maxV=Integer.MIN_VALUE;
         int minV =Integer.MAX_VALUE;
@@ -8,9 +8,7 @@ class Solution {
           maxV=Math.max(nums[i],maxV);
           minV=Math.min(nums[i],minV);
         }
-        for(int i=0;i<k;i++){
-            ans+=maxV-minV;
-        }
-        return ans +1;
+      ans = (long)k*(maxV-minV);
+        return ans ;
     }
 }
