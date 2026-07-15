1132e88da1bfdeaa47bd3a132cc2b9a6f06df318
Mehak-khann <mehakkhann2201@gmail.com>
2026-07-15 16:14:51 +0530

    Time: 1 ms (83.11%), Space: 42.8 MB (20.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,33 +1,33 @@
 class Solution {
-    // public void gcd(int a,int b,int[] val){
+    public void gcd(int a,int b,int[] val){
 
-    //     if(a==0 || b==0){
-    //         if(a>0){
-    //             val[0]=a;
-    //         }
-    //         else{
-    //             val[0]=b;
-    //         }
-    //         return;
-    //     }
-    //    if(a>b){
-    //     gcd(a%b,b,val);
-    //    }
-    //    else{
-    //     gcd(a,b%a,val);
-    //    }
-    // }
-    public void gcd(int a, int b, int[] val) {
-    // Standard base case: once the divisor (b) is 0,
-    // the current dividend (a) is the GCD.
-    if (b == 0) {
-        val[0] = a;
-        return;
+        if(a==0 || b==0){
+            if(a>0){
+                val[0]=a;
+            }
+            else{
+                val[0]=b;
+            }
+            return;
+        }
+       if(a>b){
+        gcd(a%b,b,val);
+       }
+       else{
+        gcd(a,b%a,val);
+       }
     }
-    // No need to check if a > b. 
-    // If a < b, a % b is just a, which swaps the order automatically.
-    gcd(b, a % b, val);
-}
+//     public void gcd(int a, int b, int[] val) {
+//     // Standard base case: once the divisor (b) is 0,
+//     // the current dividend (a) is the GCD.
+//     if (b == 0) {
+//         val[0] = a;
+//         return;
+//     }
+//     // No need to check if a > b. 
+//     // If a < b, a % b is just a, which swaps the order automatically.
+//     gcd(b, a % b, val);
+// }
     public int gcdOfOddEvenSums(int n) {
         int sumEven =0;
         int sumOdd=0;
